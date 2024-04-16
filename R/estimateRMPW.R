d4693525fc915d89bafeb48eff6cc917deafd4b7
Andy Shen <andyshen@Andys-MacBook-Air.local>
2024-04-16 10:18:23 -0700

    rmpw

--- R/estimateRMPW.R
+++ R/estimateRMPW.R
@@ -15,8 +15,8 @@
 estimateRMPW <- function(G, Z, Y, XA, XN, trim = 0.01, allowable = FALSE) {
   X <- cbind(XA, XN)
   if (allowable) {
-    e0 <- glm(Z ~ ., data = XA, family = binomial, weights = 1 - G, na.action = na.exclude)$fitted.values
-    e1 <- glm(Z ~ ., data = cbind(XA, XN), family = binomial, weights = G, na.action = na.exclude)$fitted.values
+    e0 <- glm(Z ~ XA, family = binomial, weights = 1 - G, na.action = na.exclude)$fitted.values
+    e1 <- glm(Z ~ X, family = binomial, weights = G, na.action = na.exclude)$fitted.values
   } else {
     e0 <- glm(Z ~ X, family = binomial, weights = 1 - G, na.action = na.exclude)$fitted.values
     e1 <- glm(Z ~ X, family = binomial, weights = G, na.action = na.exclude)$fitted.values
